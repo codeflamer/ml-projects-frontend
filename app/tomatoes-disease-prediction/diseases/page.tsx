fa9eeaf4bfd4835c651da3091a31297959503ac3
codeflamer <emryzs01@gmail.com>
2024-08-08 11:33:45 +0300

    ..

--- app/tomatoes-disease-prediction/diseases/page.tsx
+++ app/tomatoes-disease-prediction/diseases/page.tsx
@@ -5,10 +5,20 @@ import Image from "next/image";
 
 const DiseaseContent = dynamic(() => import("./disease-content.mdx"));
 
+// export async function generateStaticParams(){
+
+// }
+
 const AboutDisease = () => {
   return (
     <div className="prose mx-auto prose-slate prose-hr:my-4 prose-img:rounded-md prose-img:mx-auto  prose-p:text-justify">
       <DiseaseContent />
+      <Image
+        src="/images/tomatoes/Tomato_Target_Spot.jpg"
+        alt="Tomato Target Spot"
+        width={200}
+        height={200}
+      />
       {/* <img
         src="/images/tomatoes/Tomato_Septoria_Leaf_spot.jpg"
         alt="Shadcn Avatar"
